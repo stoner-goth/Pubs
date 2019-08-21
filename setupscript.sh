3629762478fe7ac7e1730e1f77c4014c2be3f16f
stoner-goth <51922324+stoner-goth@users.noreply.github.com>
2019-08-21 13:57:18 -0500

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo pacman -Syu
-sudo pacman -S --needed base-devel git wget yajl
+sudo pacman -S --needed base-devel git wget yajl deluge
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 yay --editmenu --nodiffmenu --save
-yay -S spotify discord betterdiscord parsec-bin
+yay -S spotify discord betterdiscord parsec-bin sabnzbd p7zip unzip par2
