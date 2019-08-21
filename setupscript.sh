df148c6abdbea12621641a04fd0cf5b5451de437
stoner-goth <51922324+stoner-goth@users.noreply.github.com>
2019-08-21 14:04:03 -0500

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -6,4 +6,4 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 yay --editmenu --nodiffmenu --save
-yay -S spotify discord betterdiscord parsec-bin sabnzbd p7zip unzip par2 realvnc-vnc-viewer qdirstat signal
+yay -S spotify discord betterdiscord parsec-bin sabnzbd p7zip unzip par2 realvnc-vnc-viewer qdirstat signal slack
